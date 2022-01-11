4df339f9748688066b6cf37d3ca9b5cc7b90d764
Anita Schiller <anita.s1@gmx.de>
2022-01-11 15:39:14 +0100

    Tightened integration between client and server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { PORT = 4000 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4000/',
+      '/api': `http://localhost:${PORT}`,
     },
   },
 });
