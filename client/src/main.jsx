4169e331b04689537b043437581ab0d20d5f66bc
Thomas Kirst <thomas@moneygarden.de>
2022-01-13 14:40:43 +0100

    Moved global CSS into Styled Component´s GlobalStyle

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyle } from './shared/GlobalStyle';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
