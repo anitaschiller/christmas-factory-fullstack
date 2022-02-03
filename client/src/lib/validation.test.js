ab2da059f8b2148b5d6d4668969a944c48995af0
Anita Schiller <anita.s1@gmx.de>
2022-02-03 10:38:35 +0100

    Changed validation test

--- client/src/lib/validation.test.js
+++ client/src/lib/validation.test.js
@@ -2,6 +2,6 @@ import { hasProductNameMinLength } from './validation';
 
 describe('validation for product', () => {
   it('has a sufficient long name', () => {
-    expect(hasProductNameMinLength('Vanillekipferl', 3)).toBe(true);
+    expect(hasProductNameMinLength('Va', 3)).toBe(true);
   });
 });
